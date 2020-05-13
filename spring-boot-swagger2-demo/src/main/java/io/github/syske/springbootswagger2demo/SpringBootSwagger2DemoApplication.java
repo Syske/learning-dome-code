c563893a5ccd53e68faf32e5b515b09d0dd3e1d2
syske <715448004@qq.com>
2020-05-13 21:44:07 +0800

    update spring-boot-swagger2-demo

--- spring-boot-swagger2-demo/src/main/java/io/github/syske/springbootswagger2demo/SpringBootSwagger2DemoApplication.java
+++ spring-boot-swagger2-demo/src/main/java/io/github/syske/springbootswagger2demo/SpringBootSwagger2DemoApplication.java
@@ -3,7 +3,7 @@ package io.github.syske.springbootswagger2demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(ex)
 public class SpringBootSwagger2DemoApplication {
 
     public static void main(String[] args) {
