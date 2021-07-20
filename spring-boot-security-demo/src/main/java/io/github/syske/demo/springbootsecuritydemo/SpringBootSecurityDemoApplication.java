1432c68970923ca0e7d35a612b03b3fb0dd396a2
syske <715448004@qq.com>
2021-07-20 08:56:25 +0800

    add spring boot security plugin

---
+++ spring-boot-security-demo/src/main/java/io/github/syske/demo/springbootsecuritydemo/SpringBootSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.syske.demo.springbootsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+    }
+
+}
---
+++ spring-boot-security-demo/src/main/java/io/github/syske/demo/springbootsecuritydemo/service/ReaderRepository.java
@@ -0,0 +1,76 @@
+/* Copyright © 2021 syske. All rights reserved. */
+package io.github.syske.demo.springbootsecuritydemo.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * @author syske
+ * @version 1.0
+ * @date 2021-07-20 7:58
+ */
+@Service
+public class ReaderRepository implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if ("admin".equals(username)) {
+            return new UserInfo("admin", new BCryptPasswordEncoder()
+                    .encode("admin"));
+        } else {
+            return null;
+        }
+    }
+
+    public static class UserInfo implements UserDetails {
+        private String username;
+        private String password;
+
+        public UserInfo(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return Arrays.asList(new SimpleGrantedAuthority("READER"));
+        }
+
+        @Override
+        public String getPassword() {
+            return this.password;
+        }
+
+        @Override
+        public String getUsername() {
+            return this.username;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+    }
+}
