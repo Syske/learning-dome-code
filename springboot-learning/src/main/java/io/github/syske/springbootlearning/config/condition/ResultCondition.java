6eda8a28f470dfd3f007ef774c97ff589bd9b3ca
syske <715448004@qq.com>
2021-09-22 08:57:23 +0800

    增加条件配置：Condition相关内容

---
+++ springboot-learning/src/main/java/io/github/syske/springbootlearning/config/condition/ResultCondition.java
@@ -0,0 +1,22 @@
+/* Copyright © 2021 syske. All rights reserved. */
+package io.github.syske.springbootlearning.config.condition;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+/**
+ * 条件配置
+ *
+ * @author syske
+ * @version 1.0
+ * @date 2021-09-22 8:35
+ */
+public class ResultCondition implements Condition {
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        Boolean matches = true;
+        System.out.println("ResultCondition matches =" + matches);
+        return matches;
+    }
+}
