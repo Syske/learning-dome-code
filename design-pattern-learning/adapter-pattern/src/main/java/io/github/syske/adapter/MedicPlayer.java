89e317f5405b4342ae481a5ad68bceb6404ffe0d
lei.cao <lei.cao@coolcollege.cn>
2021-10-14 20:15:25 +0800

    更新设计模式相关代码

---
+++ design-pattern-learning/adapter-pattern/src/main/java/io/github/syske/adapter/MedicPlayer.java
@@ -0,0 +1,11 @@
+package io.github.syske.adapter;
+
+/**
+ * @program: design-mode-learning
+ * @description:
+ * @author: syske
+ * @date: 2021-10-13 13:37
+ */
+public interface MedicPlayer {
+    void play(String audioType, String fileName);
+}
