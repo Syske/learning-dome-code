d8202cdff1e7569651b872b89253f6dafd35e4d4
syske <715448004@qq.com>
2021-11-04 08:45:09 +0800

    update note

---
+++ spring-boot-sentinel-demo/src/main/java/io/github/syske/springbootsentineldemo/SpringBootSentinelDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.syske.springbootsentineldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSentinelDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSentinelDemoApplication.class, args);
+    }
+
+}
---
+++ spring-boot-sentinel-demo/src/test/java/io/github/syske/springbootsentineldemo/SpringBootSentinelDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.syske.springbootsentineldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSentinelDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
